5946700009f33eb224b1b67a97cc425ea8e00952
LeMinhMan2809 <lemann.cth@gmail.com>
2024-11-08 15:43:45 +0700

    Modify API for order

--- src/utils/orderAPI.js
+++ src/utils/orderAPI.js
@@ -1,5 +1,28 @@
 import axios from "axios";
 
+export const getOrderByOrderID = async (url) => {
+  try {
+    const res = await axios.get("http://localhost:4000" + url);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getOrderByUserID = async (url, page, pageSize) => {
+  try {
+    const res = await axios.get("http://localhost:4000" + url, {
+      params: {
+        page,
+        pageSize,
+      },
+    });
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export const addOrderAPI = async (url, formData) => {
   try {
     const res = await axios.post("http://localhost:4000" + url, formData);
@@ -18,4 +41,21 @@ export const getOrderStatusAPI = async (url) => {
   }
 };
 
+// OrderDetail
+export const addOrderDetailAPI = async (url, formData) => {
+  try {
+    const res = await axios.post("http://localhost:4000" + url, formData);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
+export const getOrderDetailByOrderIDAPI = async (url) => {
+  try {
+    const res = await axios.get("http://localhost:4000" + url);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
