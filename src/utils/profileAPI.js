e2614e2f91fc07c0096e0b130525b86fca212022
LeMinhMan2809 <lemann.cth@gmail.com>
2024-11-08 15:44:02 +0700

    Modify API for user profile page

--- src/utils/profileAPI.js
+++ src/utils/profileAPI.js
@@ -10,16 +10,34 @@ export const getUserProfileAPI = async (url) => {
 
 export const getUserProfileByIDAPI = async (url, id) => {
   try {
-    const res = await axios.get("http://localhost:4000" + url + id);
+    const res = await axios.get("http://localhost:4000" + url, id);
     return res.data;
   } catch (error) {
     console.log(error);
   }
 };
 
-export const updateProfileAPI = async (url, updatedData) => {
+export const updateProfileAPI = async (id, updatedData) => {
   try {
-    const res = await axios.put("http://localhost:4000" + url, updatedData);
+    const res = await axios.put("http://localhost:4000" + id, updatedData);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const forgotPasswordAPI = async (url, formData) => {
+  try {
+    const res = await axios.post("http://localhost:4000" + url, formData);
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const resetPasswordAPI = async (url, formData) => {
+  try {
+    const res = await axios.post("http://localhost:4000" + url, formData);
     return res.data;
   } catch (error) {
     console.log(error);
