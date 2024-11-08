811f7bc07614e1d670510d9a909e1b9759232a64
LeMinhMan2809 <lemann.cth@gmail.com>
2024-11-08 15:32:20 +0700

    Modify home page (swiper and loading)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import Banner from "./Banner";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 import { StoreContext } from "../context/StoreContext";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Link } from "react-router-dom";
@@ -14,54 +16,87 @@ import { Pagination, Navigation } from "swiper/modules";
 import ProductCard from "../components/ProductCard";
 
 import warrantyIcon from "../assets/warranty.png";
-import { listProducts } from "../utils/handleAPI";
+import { listProducts, listCategories } from "../utils/handleAPI";
 
 const Home = () => {
+  const maxPaginationBullets = 5; // Set the limit for pagination bullets
+
   const { url, token, setToken, isFocused } = useContext(StoreContext);
+  const [loading, setLoading] = useState(true);
+  const [productDataSwiper, setProductDataSwiper] = useState([]);
   const [productData, setProductData] = useState([]);
+  const [categoryData, setCategoryData] = useState([]);
 
   useEffect(() => {
-    listProducts("/api/product").then((res) => {
-      setProductData(res);
+    listCategories("/api/category").then((res) => {
+      setCategoryData(res);
+      setLoading(false);
     });
   }, []);
+
+  //Get products by category
+  useEffect(() => {
+    listProducts("/api/product").then((res) => {
+      setProductDataSwiper(res.slice(0, 8));
+      setLoading(false);
+    });
+    if (categoryData && categoryData.length > 0) {
+      // Ensure categoryData is populated before making the API call
+      listProducts("/api/product/category/" + categoryData[0]?._id).then(
+        (res) => {
+          setProductData(res.slice(0, 8));
+          setLoading(false);
+        }
+      );
+    }
+  }, [categoryData]);
 
   return (
     <div className={`page-container ${isFocused ? "blurred" : ""}`}>
       <Banner />
 
-      <div className="mt-5 ml-[80px] mr-[80px]">
+      <div className="mt-5 ml-[80px] mr-[80px] bg-white">
         <h3 className="text-3xl pb-5">Bán chạy nhất</h3>
 
         <Swiper
           slidesPerView={4}
           spaceBetween={30}
-          navigation={true}
+          // navigation={true}
+          navigation={{
+            prevEl: ".swiper-button-prev",
+            nextEl: ".swiper-button-next",
+          }}
           pagination={{
             clickable: true,
+            dynamicBullets: true,
+            // renderBullet: (index, className) => {
+            //   // Render only the first `maxPaginationBullets` bullets
+            //   if (index < maxPaginationBullets) {
+            //     return `<span class="${className}"></span>`;
+            //   }
+            //   return ""; // Return an empty string for bullets beyond the limit
+            // },
           }}
           modules={[Pagination, Navigation]}
           className="mySwiper"
+          style={{
+            "--swiper-navigation-color": "white",
+            "--swiper-pagination-color": "green",
+            "--swiper-navigation-size": "5rem",
+            "--swiper-pagination-margin-top": "20px",
+          }}
         >
-          <SwiperSlide>
-            <ProductCardSwiper />
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <ProductCardSwiper />
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <ProductCardSwiper />
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <ProductCardSwiper />
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <ProductCardSwiper />
-          </SwiperSlide>
+          {productDataSwiper.map((product, index) => (
+            <SwiperSlide key={index}>
+              <ProductCardSwiper
+                key={index}
+                name={product.name}
+                images={product.images}
+                price={product.price}
+                description={product.description}
+              />
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
 
@@ -79,6 +114,20 @@ const Home = () => {
               />
             </Link>
           ))}
+        </div>
+        <div className="mt-6 text-center text-xl font-semibold border-[#E1D7C6] border-2 cursor-pointer hover:bg-orange-300 py-2 rounded-full">
+          {loading ? (
+            <Box
+              className="flex justify-center items-center mt-10"
+              sx={{ display: "flex" }}
+            >
+              <CircularProgress />
+            </Box>
+          ) : (
+            <Link to={`/category/${categoryData[0]._id}`}>
+              Xem thêm sản phẩm
+            </Link>
+          )}
         </div>
       </div>
 
